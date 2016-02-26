44cb506915f9bf0bc2dd754a575cab8a11dea9bb
Anna Selezniova <ask.dev@gmail.com>
2016-02-26 13:59:43 +0300

    Separate component for rotator demo

--- front/postcss/plugins.js
+++
@@ -1,9 +0,0 @@
-const autoreset       = require('postcss-autoreset');
-const nested          = require('postcss-nested');
-const autoprefixer    = require('autoprefixer');
-
-module.exports = [
-  nested(),
-  autoreset(),
-  autoprefixer()
-];
